c830e88c4c805b5af99938c6656a4b5717b664b6
frank-yuan <felk2005@gmail.com>
2016-07-25 23:29:26 +0800

    initial commit

---
+++ Nanodegree_OnTheMap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Nanodegree_OnTheMap
+//
+//  Created by Xuan Yuan (Frank) on 7/25/16.
+//  Copyright © 2016 frank-yuan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
