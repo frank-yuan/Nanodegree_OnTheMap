fe4c70951db9f629a6b59c05b5d22fc3da2ff489
frank-yuan <felk2005@gmail.com>
2016-08-03 23:24:00 +0800

    fix: beautify code

--- Nanodegree_OnTheMap/LocationEditViewController.swift
+++ Nanodegree_OnTheMap/LocationEditViewController.swift
@@ -9,8 +9,9 @@
 import UIKit
 import MapKit
 
-class LocationEditViewController: UIViewController , CLLocationManagerDelegate{
+class LocationEditViewController: UIViewController , CLLocationManagerDelegate, MKMapViewDelegate{
 
+    // MARK: IBOutlet
     @IBOutlet weak var stackView : UIStackView!
     @IBOutlet weak var locationTextField : UITextField!
     @IBOutlet weak var linkTextField : UITextField!
@@ -18,33 +19,38 @@ class LocationEditViewController: UIViewController , CLLocationManagerDelegate{
     @IBOutlet weak var searchButton : UIButton!
     @IBOutlet weak var submitButton : UIButton!
     
-    
-    var objectId = ""
-    private var localSearch:MKLocalSearch? = nil
+    // MARK: Constant
     private let textDelegate = TextFieldDelegate()
     private let locationManager = CLLocationManager()
-    private var userLocation = CLLocationCoordinate2D()
     private let coordinateSpan = MKCoordinateSpan(latitudeDelta: 0.112872, longitudeDelta: 0.109863)
     
+    // MARK: Fields
+    var objectId = ""
+    private var localSearch:MKLocalSearch? = nil
+    private var userLocation = CLLocationCoordinate2D()
+    
+    
+    // MARK: UIViewController overrides
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         locationTextField.delegate = textDelegate
         linkTextField.delegate = textDelegate
         
         configureUI(false)
-        // Do any additional setup after loading the view.
+        // Tap anywhere to end editing
         let tapper = UITapGestureRecognizer(target: self, action: #selector(handleSingleTap(_:)))
         tapper.cancelsTouchesInView = false
         view.addGestureRecognizer(tapper);
     }
     
+    // MARK: IBActions
     @IBAction func onCancel() {
         dismissViewControllerAnimated(true, completion: nil)
     }
     
     @IBAction func onSearch() {
         
-        locationTextField.resignFirstResponder()
         if locationTextField.text?.characters.count == 0 {
             showAlert("Type some thing to search", message: "", buttonText: "OK")
             return
@@ -59,6 +65,7 @@ class LocationEditViewController: UIViewController , CLLocationManagerDelegate{
         
         localSearch = MKLocalSearch(request: request)
         localSearch!.startWithCompletionHandler(){ (response, error) in
+            
             if let error = error {
                 self.onLocationNotFound(error)
             } else {
@@ -70,7 +77,7 @@ class LocationEditViewController: UIViewController , CLLocationManagerDelegate{
     }
     
     @IBAction func onSubmit() {
-        locationTextField.resignFirstResponder()
+        
         if linkTextField.text?.characters.count == 0 {
             showAlert("Type your linkedin address", message: "", buttonText: "OK")
             return
@@ -117,16 +124,25 @@ class LocationEditViewController: UIViewController , CLLocationManagerDelegate{
         //configureUI(true)
     }
     
-    func configureUI(enableMapView:Bool) {
-        stackView.hidden = enableMapView
-        locationTextField.hidden = enableMapView
-        linkTextField.hidden = !enableMapView
-        mapView.hidden = !enableMapView
-        searchButton.hidden = enableMapView
-        submitButton.hidden = !enableMapView
+    // MARK: Delegate implementation of MKMapViewDelegate
+    func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        let reuseId = "pin"
+        
+        var pinView = mapView.dequeueReusableAnnotationViewWithIdentifier(reuseId) as? MKPinAnnotationView
+        
+        if pinView == nil {
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+            pinView!.pinTintColor = UIColor.redColor()
+        }
+        else {
+            pinView!.annotation = annotation
+        }
+        
+        return pinView
     }
     
-    // MARK: Implement of UITextFieldDelegate
+    // MARK: Implement of CLLocationManagerDelegate
     func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         if locations.count > 0 {
             let location = locations.last!
@@ -147,11 +163,19 @@ class LocationEditViewController: UIViewController , CLLocationManagerDelegate{
         }
     }
     
-    func onLocationNotFound(error:NSError?) {
-        showAlert("Could not find places", message: error == nil ? "" : error!.userInfo.description, buttonText: "OK")
+    
+    // MARK: Private functions
+    
+    private func configureUI(enableMapView:Bool) {
+        stackView.hidden = enableMapView
+        locationTextField.hidden = enableMapView
+        linkTextField.hidden = !enableMapView
+        mapView.hidden = !enableMapView
+        searchButton.hidden = enableMapView
+        submitButton.hidden = !enableMapView
     }
     
-    func configureMapView(coordinate: CLLocationCoordinate2D) {
+    private func configureMapView(coordinate: CLLocationCoordinate2D) {
         userLocation = coordinate
         mapView.setCenterCoordinate(coordinate, animated: true)
         mapView.setRegion(MKCoordinateRegion(center: coordinate, span: coordinateSpan), animated: true)
@@ -160,33 +184,18 @@ class LocationEditViewController: UIViewController , CLLocationManagerDelegate{
         mapView.addAnnotation(annotation)
     }
     
-    func handleSingleTap(sender:UITapGestureRecognizer){
+    @objc private func handleSingleTap(sender:UITapGestureRecognizer){
         view.endEditing(true)
     }
     
-    func showAlert(title: String, message: String, buttonText: String) {
+    private func onLocationNotFound(error:NSError?) {
+        showAlert("Could not find places", message: error == nil ? "" : error!.userInfo.description, buttonText: "OK")
+    }
+    
+    private func showAlert(title: String, message: String, buttonText: String) {
         
             let alert = UIAlertController(title: title, message: message, preferredStyle: .Alert)
             alert.addAction(UIAlertAction(title: buttonText, style: .Default, handler: nil))
             presentViewController(alert, animated: true, completion: nil)
     }
-    
-    func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
-        
-        let reuseId = "pin"
-        
-        var pinView = mapView.dequeueReusableAnnotationViewWithIdentifier(reuseId) as? MKPinAnnotationView
-        
-        if pinView == nil {
-            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
-            pinView!.pinTintColor = UIColor.redColor()
-        }
-        else {
-            pinView!.annotation = annotation
-        }
-        
-        return pinView
-    }
-
-
 }
